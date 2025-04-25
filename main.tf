6b8aabbd1f77b81c8f7725ea321a9021a8813036
Farrah Amador-Mughal <farrah@dnastack.com>
2025-04-25 16:11:02 -0400

    removing kubernetes provider

--- main.tf
+++ main.tf
@@ -33,15 +33,6 @@ data "azurerm_virtual_network" "vnet" {
   name                = var.virtualNetworkName
 }
 
-
-provider "kubernetes" {
-  host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
-
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
-}
-
 resource "random_string" "namespace_suffix" {
   length  = 8
   special = false
